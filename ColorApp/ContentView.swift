2b3312fecd1377afb68ea2737bacf125b80e1900
BerdinaKatya <kuznkate42@gmail.com>
2023-10-16 20:11:41 +0500

    setup sliders and color view

--- ColorApp/ContentView.swift
+++ ColorApp/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color
+                .gray
+                .ignoresSafeArea()
+                .opacity(0.3)
+            
+            VStack {
+                Color(
+                    red: redSliderValue / 255,
+                    green: greenSliderValue / 255,
+                    blue: blueSliderValue / 255
+                )
+                    .frame(width: 320, height: 180)
+                    .clipShape(.rect(cornerRadius: 30))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.white, lineWidth: 5)
+                    )
+                    .padding(.bottom, 30)
+                
+                ColorSlideView(value: $redSliderValue, color: .red)
+                ColorSlideView(value: $greenSliderValue, color: .green)
+                ColorSlideView(value: $blueSliderValue, color: .blue)
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ColorSlideView: View {
+    @Binding var value: Double
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .frame(width: 40)
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
+        }
+    }
 }
