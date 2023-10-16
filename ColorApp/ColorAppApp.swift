dc714782b348309f8e593f03f5ee149f3bdc4ae2
BerdinaKatya <kuznkate42@gmail.com>
2023-10-16 19:28:18 +0500

    Initial Commit

---
+++ ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Екатерина Теляубердина on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
